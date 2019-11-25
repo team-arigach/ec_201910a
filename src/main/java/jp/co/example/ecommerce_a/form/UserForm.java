e143a95e2e6668b9d74caea822c12d8d7f8075e6
yosukeYamada <yamada_141@yahoo.co.jp>
2019-11-25 20:24:10 +0900

    UserFormを追加

---
+++ src/main/java/jp/co/example/ecommerce_a/form/UserForm.java
@@ -0,0 +1,55 @@
+package jp.co.example.ecommerce_a.form;
+
+public class UserForm {
+	
+	private String name;
+	private String email;
+	private String telephone;
+	private String password;
+	private String passwordconfomation;
+	private String address;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPasswordconfomation() {
+		return passwordconfomation;
+	}
+	public void setPasswordconfomation(String passwordconfomation) {
+		this.passwordconfomation = passwordconfomation;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "UserForm [name=" + name + ", email=" + email + ", telephone=" + telephone + ", password=" + password
+				+ ", passwordconfomation=" + passwordconfomation + ", address=" + address + "]";
+	}
+	
+	
+
+}
