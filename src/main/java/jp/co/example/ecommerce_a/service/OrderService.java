f88a82fb0ea9e72d90401669f3c988b5993bcd38
yuki-and <yuki.andoooo@gmail.com>
2019-11-26 15:51:41 +0900

    支払者情報を更新するメソッド追加

---
+++ src/main/java/jp/co/example/ecommerce_a/service/OrderService.java
@@ -0,0 +1,26 @@
+package jp.co.example.ecommerce_a.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.example.ecommerce_a.domain.Order;
+import jp.co.example.ecommerce_a.repository.OrderRepository;
+
+@Service
+@Transactional
+public class OrderService {
+
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	public void order(Order order) {
+		orderRepository.findByUserIdAndStatus(order.getUserId(), 0);
+		if(order.getPaymentMethod() == 1) {
+			order.setStatus(1);
+		}else if(order.getPaymentMethod() == 2) {
+			order.setStatus(2);
+		}
+		orderRepository.update(order);
+	}
+}
