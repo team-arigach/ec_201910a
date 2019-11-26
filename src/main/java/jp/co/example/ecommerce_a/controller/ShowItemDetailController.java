0b948b369264bd86839cfd2e43130ca2d5e47f82
yuki-and <yuki.andoooo@gmail.com>
2019-11-26 10:59:04 +0900

    商品詳細表示のコントローラ実装

---
+++ src/main/java/jp/co/example/ecommerce_a/controller/ShowItemDetailController.java
@@ -0,0 +1,44 @@
+package jp.co.example.ecommerce_a.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.example.ecommerce_a.service.ShowItemDetailService;
+
+/**
+ * 商品詳細表示の処理を行うコントローラ.
+ * 
+ * @author yukiando
+ *
+ */
+@Controller
+@RequestMapping("detail")
+public class ShowItemDetailController {
+
+	@Autowired
+	private ShowItemDetailService showItemDetailService;
+	
+	/**
+	 * 商品詳細を表示します.
+	 * 
+	 * @param id ID
+	 * @return 商品詳細ページ
+	 */
+	@RequestMapping("")
+	public String showItemDetail(Integer id) {
+		showItemDetailService.showItemDetail(id);
+		return "item_detail";
+	}
+	
+	/**
+	 * ショッピングカートへ遷移します.
+	 * 
+	 * @return　ショッピングカートページ
+	 */
+	@RequestMapping("toShoppingCart")
+	public String toShoppingCart() {
+		return "cart_list";
+	}
+	
+}
