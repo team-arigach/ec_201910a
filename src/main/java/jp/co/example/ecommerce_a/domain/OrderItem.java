575ed90a6ee4db312990fa5f64132ac67b13396c
suzuki-ryouhei <ryohei.suzuki@rakus-partners.co.jp>
2019-11-28 15:53:39 +0900

    小計を表示させました。

--- src/main/java/jp/co/example/ecommerce_a/domain/OrderItem.java
+++ src/main/java/jp/co/example/ecommerce_a/domain/OrderItem.java
@@ -23,60 +23,87 @@ public class OrderItem {
 	private Item item;
 	/** 注文トッピングリスト */
 	private List<OrderTopping> orderToppingList;
-	
+
 	public int getSubTotal() {
-		return 0; // 警告を消すために記述
+		Integer toppingItems = 0;
+		if (orderToppingList != null) {
+			toppingItems = this.orderToppingList.size();
+		}
+		Integer subTotal = 0;
+		if (this.size == 'M') {
+			Integer toppingPrice = 200;
+			subTotal = (toppingItems * toppingPrice) + (item.getPriceM() * this.quantity);
+		}
+		if (this.size == 'L') {
+			Integer toppingPrice = 300;
+			subTotal = (toppingItems * toppingPrice) + (item.getPriceL() * this.quantity);
+
+		}
+
+		return subTotal;
 	}
-	
-	// 以下、 getter / setter 
+
+	// 以下、 getter / setter
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public Integer getItemId() {
 		return itemId;
 	}
+
 	public void setItemId(Integer itemId) {
 		this.itemId = itemId;
 	}
+
 	public Integer getOrderId() {
 		return orderId;
 	}
+
 	public void setOrderId(Integer orderId) {
 		this.orderId = orderId;
 	}
+
 	public Integer getQuantity() {
 		return quantity;
 	}
+
 	public void setQuantity(Integer quantity) {
 		this.quantity = quantity;
 	}
+
 	public Character getSize() {
 		return size;
 	}
+
 	public void setSize(Character size) {
 		this.size = size;
 	}
+
 	public Item getItem() {
 		return item;
 	}
+
 	public void setItem(Item item) {
 		this.item = item;
 	}
+
 	public List<OrderTopping> getOrderToppingList() {
 		return orderToppingList;
 	}
+
 	public void setOrderToppingList(List<OrderTopping> orderToppingList) {
 		this.orderToppingList = orderToppingList;
 	}
+
 	@Override
 	public String toString() {
 		return "OrderItem [id=" + id + ", itemId=" + itemId + ", orderId=" + orderId + ", quantity=" + quantity
 				+ ", size=" + size + ", item=" + item + ", orderToppingList=" + orderToppingList + "]";
 	}
-	
-	
 
 }
