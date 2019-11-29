744d1cb4754c4c154eecbb964b54ae6fa5fb1785
su-san1129 <deviationvalue1129@icloud.com>
2019-11-29 09:48:43 +0900

    依存関係修正

--- src/main/java/jp/co/example/ecommerce_a/controller/UserController.java
+++ src/main/java/jp/co/example/ecommerce_a/controller/UserController.java
@@ -1,14 +1,10 @@
 package jp.co.example.ecommerce_a.controller;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import jp.co.example.ecommerce_a.domain.User;
-import jp.co.example.ecommerce_a.repository.UserRepository;
 
 /**
  * ユーザーを操作するコントローラー.
