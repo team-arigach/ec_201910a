ce2423e6856477b51c243e756939c375abb4ffa4
suzuki-ryouhei <ryohei.suzuki@rakus-partners.co.jp>
2019-11-30 20:00:12 +0900

    商品確認画面の削除ボタン追加

--- src/main/java/jp/co/example/ecommerce_a/controller/ShowConfirmController.java
+++ src/main/java/jp/co/example/ecommerce_a/controller/ShowConfirmController.java
@@ -1,15 +1,15 @@
 package jp.co.example.ecommerce_a.controller;
 
-
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.example.ecommerce_a.domain.LoginUser;
 import jp.co.example.ecommerce_a.domain.Order;
 import jp.co.example.ecommerce_a.form.OrderForm;
 import jp.co.example.ecommerce_a.service.ShowConfirmService;
@@ -27,7 +27,6 @@ public class ShowConfirmController {
 	@Autowired
 	private ShowConfirmService showConfirmService;
 	
-	
 	@Autowired
 	private HttpSession session;
 	
@@ -44,11 +43,19 @@ public class ShowConfirmController {
 	 * @return　商品確認画面
 	 */
 	@RequestMapping("/showConfirm")
-	public String showConfirm(Model model) {
+	public String showConfirm(Model model, @AuthenticationPrincipal LoginUser loginUser) {
 		Integer userId = session.getId().hashCode();
+		if(loginUser != null) {
+			userId = loginUser.getUser().getId();
+		} else {
+			userId = session.getId().hashCode();
+		}
 		Order order = showConfirmService.showOrderConfirm(userId, 0);
+		System.out.println(order);
 		if(order != null) {
+			if(order.getOrderItemList().size() != 0) {
 			model.addAttribute("order", order);
+			}
 		}
 		return "order_confirm"; 
 	}
